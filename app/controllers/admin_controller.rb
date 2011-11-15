036471f3b4e2514d59f81ae8ee2009d3ac55dfbf
Hank Stoever <hstove@gmail.com>
2011-11-15 11:04:02 -0800

    deleted bootstrap files

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,5 @@
 class AdminController < ApplicationController
-  before_filter :ensure_admin
+  #before_filter :ensure_admin
   
   def index
     @posts = Post.all.sort { |a,b| -(a.score <=> b.score)}
